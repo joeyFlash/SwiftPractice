70c6a86d54fd7926154d449904203d2a9f3ffce5
Joey <joey.hernandez33@gmail.com>
2016-04-15 10:39:29 -0600

    Created Tables

--- TimesTableApp/TimesTableApp/ViewController.swift
+++ TimesTableApp/TimesTableApp/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate {
 
+    
+    var cellContent = ["joeyFlash", "superman", "wolverine", "deadpool"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return cellContent.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        return cell
+    }
 }
 
