96a25dda8c42582f9aee1bf36c4d5b4dcc2747f3
Joey <joey.hernandez33@gmail.com>
2016-05-02 01:06:26 -0600

    added code to make all noughts into x

--- TicTacToeApp/TicTacToeApp/ViewController.swift
+++ TicTacToeApp/TicTacToeApp/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button: UIButton!
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var image = UIImage(named: "cross.png")
+        
+        sender.setImage(image, forState: .Normal)
+        
+        print(sender.tag)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
