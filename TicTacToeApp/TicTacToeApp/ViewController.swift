c887f3becb71fbc2a27391c4c3b7af3aa6a428cd
Joey <joey.hernandez33@gmail.com>
2016-05-03 22:30:47 -0600

    added game functionality. Tells the console who wins and stops the game.

--- TicTacToeApp/TicTacToeApp/ViewController.swift
+++ TicTacToeApp/TicTacToeApp/ViewController.swift
@@ -12,14 +12,61 @@ class ViewController: UIViewController {
 
     @IBOutlet var button: UIButton!
     
+    var activePlayer = 1
+    
+    var gameActive = true
+    
+    var gameBoard = [0,0,0,0,0,0,0,0,0]
+    
+    var winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     
     @IBAction func buttonPressed(sender: AnyObject) {
         
-        var image = UIImage(named: "cross.png")
         
-        sender.setImage(image, forState: .Normal)
-        
-        print(sender.tag)
+        if(gameBoard[sender.tag] == 0 && gameActive)
+        {
+            var image = UIImage()
+            
+            //updates the gameboard to whats been pressed
+            gameBoard[sender.tag] = activePlayer
+            
+            if(activePlayer == 1)
+            {
+                image = UIImage(named: "nought.png")!
+                
+                activePlayer = 2
+            }
+            else
+            {
+                image = UIImage(named: "cross.png")!
+                
+                activePlayer = 1
+                
+            }
+            
+            
+            for combo in winningCombos
+            {
+               if(gameBoard[combo[0]] != 0 && gameBoard[combo[0]] == gameBoard[combo[1]] && gameBoard[combo[1]] == gameBoard[combo[2]])
+               {
+                
+                if(gameBoard[combo[0]] == 1)
+                {
+                    print("Noughts have won")
+                }
+                else
+                {
+                    print("Crosses have one")
+                }
+                
+                 gameActive = false
+               }
+                
+            }
+            sender.setImage(image, forState: .Normal)
+            print(sender.tag)
+
+        }
         
     }
     
