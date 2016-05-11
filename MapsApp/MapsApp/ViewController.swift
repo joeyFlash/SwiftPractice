b9f618ada200f7f0f3d78f6b78ddc80a69d59fbf
Joey <joey.hernandez33@gmail.com>
2016-05-10 21:05:13 -0600

    Created initial Map View

---
+++ MapsApp/MapsApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MapsApp
+//
+//  Created by Joseph Hernandez on 5/10/16.
+//  Copyright © 2016 Skgood Inc. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
