fda095aa2c46a75016229e2ee68471fa85c2df4a
Joey <joey.hernandez33@gmail.com>
2016-05-18 01:29:45 -0600

    Added annotations for every long press

--- MapsApp/MapsApp/ViewController.swift
+++ MapsApp/MapsApp/ViewController.swift
@@ -19,15 +19,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         var latitude: CLLocationDegrees = 35.172910
+        
         var longitude: CLLocationDegrees = -106.590837
+        
         var latDelta: CLLocationDegrees = 0.01
+        
         var longDelta: CLLocationDegrees = 0.01
-        var span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
         var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
         var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
         mapView.setRegion(region, animated: true)
+        
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Ultramain Systems"
+        annotation.subtitle = "I work here!"
+        mapView.addAnnotation(annotation)
+        
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 2
+        mapView.addGestureRecognizer(uilpgr)
     }
 
+    
+    func action(gestureRecognizer: UIGestureRecognizer)
+    {
+        print("Gesture Recgonized")
+        var touchPoint = gestureRecognizer.locationInView(self.mapView)
+        var newCoordinate: CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinate
+        annotation.title = "New Place"
+        annotation.subtitle = "I dont work here!"
+        mapView.addAnnotation(annotation)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
