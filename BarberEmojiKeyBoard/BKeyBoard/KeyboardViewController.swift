b31a776eda99462b25197cfe87facb583926f1a7
Joey <joey.hernandez33@gmail.com>
2016-05-27 00:48:07 -0600

    created Map View Controller

--- BarberEmojiKeyBoard/BKeyBoard/KeyboardViewController.swift
+++ BarberEmojiKeyBoard/BKeyBoard/KeyboardViewController.swift
@@ -12,6 +12,12 @@ class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
 
+    @IBOutlet var deleteButton: UIButton!
+    
+    @IBOutlet var hideKeyboardButton: UIButton!
+    
+    var customInterface: UIView!
+    
     override func updateViewConstraints() {
         super.updateViewConstraints()
     
@@ -20,20 +26,23 @@ class KeyboardViewController: UIInputViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .System)
-    
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-    
-        self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
         
-        self.view.addSubview(self.nextKeyboardButton)
-    
-        self.nextKeyboardButton.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
-        self.nextKeyboardButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
+        view.addSubview(customInterface)
+        
+        // Perform custom UI setup here
+        addNextKeyboardButton()
+//        self.nextKeyboardButton = UIButton(type: .System)
+//    
+//        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+//        self.nextKeyboardButton.sizeToFit()
+//        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+//    
+//        self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+//        
+//        self.view.addSubview(self.nextKeyboardButton)
+//    
+//        self.nextKeyboardButton.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
+//        self.nextKeyboardButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,5 +66,85 @@ class KeyboardViewController: UIInputViewController {
         }
         self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
     }
+    
+    func addNextKeyboardButton()
+    {
+        self.nextKeyboardButton = UIButton(type: .System)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
+        self.nextKeyboardButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
+    }
+    
+    //deletes button will use the deleteBackward from the textDocumentProxy to delete a character when pressed.
+    func addDelete()
+    {
+        
+        self.deleteButton = UIButton(type: .System)
+        self.deleteButton.setTitle(NSLocalizedString("Delete", comment: "Title for 'Delete' button"), forState: .Normal)
+        self.deleteButton.sizeToFit()
+        self.deleteButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.deleteButton.addTarget(self, action: #selector(KeyboardViewController.didTapDelete), forControlEvents: .TouchUpInside)
+        
+        self.deleteButton.backgroundColor = UIColor(white: 0.9, alpha: 1)
+        self.deleteButton.layer.cornerRadius = 5
+        
+        self.view.addSubview(self.deleteButton)
+        
+        self.deleteButton.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor).active = true
+        self.deleteButton.topAnchor.constraintEqualToAnchor(self.view.topAnchor).active = true
 
+        
+    }
+
+    func didTapDelete()
+    {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        
+        proxy.deleteBackward()
+    }
+    
+    //hideKeyBoard will call dismisskeyBoard on the keyboardviewontroller when pressed
+    func addHideKeyboardButton()
+    {
+        self.hideKeyboardButton = UIButton(type: .System)
+        self.hideKeyboardButton.setTitle(NSLocalizedString("Hide keyboard", comment: "Title for 'Hidekeyboard' button"), forState: .Normal)
+        self.hideKeyboardButton.sizeToFit()
+        self.hideKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.hideKeyboardButton.addTarget(self, action: #selector(UIInputViewController.dismissKeyboard), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.hideKeyboardButton)
+        
+        self.hideKeyboardButton.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor).active = true
+        self.hideKeyboardButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?)
+    {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        let nib = UINib(nibName: "CustomKeyboardInterface", bundle: nil)
+        let objects = nib.instantiateWithOwner(self, options: nil)
+        customInterface = objects[0] as! UIView
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+  
+    
+    @IBAction func didTapWEmoji() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        
+        proxy.insertText("We ❤ Swift")
+    }
 }
